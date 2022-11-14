f4b5a507d0df7edf41a0baf46ef2950ebdb86138
Austin Papritz <austin@papritz.dev>
2022-11-14 13:51:22 -0800

    dog cards show on pageload, cards are clickable but deails page is broken

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ const dogListContainer = document.getElementById('dog-list-container');
 // on load
 // fetch all dogs
 // render and append all dog cards to the container
-window.addEventListener('load', async () => {});
+window.addEventListener('load', async () => {
+    const dogs = await getDogs();
+    for (let dog of dogs) {
+        const dogsEl = renderDogCard(dog);
+        dogListContainer.append(dogsEl);
+    }
+});
